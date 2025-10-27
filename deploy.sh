48e65944fbc9fedfe3675edf5b7dfdcc24cd3071
XiaoweiDev2025 <software.dev.2025.go@gmail.com>
2025-10-27 14:20:37 +0000

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,12 @@ npm ci
 mkdir -p dist/client
 cp -r ../client/dist/* dist/client/
 
-echo "=== Listing @prisma/client/runtime ==="
-ls -R node_modules/@prisma/client/runtime || echo "Directory not found"
+#echo "=== Listing @prisma/client/runtime ==="
+#ls -R node_modules/@prisma/client/runtime || echo "Directory not found"
 
 npx prisma generate --schema prisma/schema.prisma
 npx prisma migrate deploy --schema prisma/schema.prisma
+cd server
 npm run build
 
 echo "Deploy script finished successfully"
